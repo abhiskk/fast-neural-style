c062d6ba91b7fb55a8928235a3f8955eee331548
darkstar112358 <abhishekkadiyan@gmail.com>
2017-03-14 10:33:52 -0400

    Refactor.

--- neuralstyle/utils.py
+++ neuralstyle/utils.py
@@ -1,19 +1,17 @@
 import os
-
-from torch.utils.serialization import load_lua
-
 import torch
-import transformernet as net
+from torch.utils.serialization import load_lua
+from vgg16 import Vgg16
 
 
-def init_vgg16(model_folder='model'):
+def init_vgg16(model_folder):
     """load the vgg16 model feature"""
     if not os.path.exists(model_folder + '/vgg16.weight'):
         if not os.path.exists(model_folder + '/vgg16.t7'):
             os.system(
                 'wget http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/vgg16.t7 ' + model_folder + '/vgg16.t7')
         vgglua = load_lua(model_folder + '/vgg16.t7')
-        vgg = net.Vgg16Part()
+        vgg = Vgg16()
         for (src, dst) in zip(vgglua.parameters()[0], vgg.parameters()):
             dst[:] = src[:]
         torch.save(vgg.state_dict(), model_folder + '/vgg16.weight')
