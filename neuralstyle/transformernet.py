Fix tanh scaling and add helper methods

neuralstyle/transformernet.py
@@ -48,7 +48,7 @@ class TransformerNet(torch.nn.Module):
         y = self.relu(self.in4(self.deconv1(y)))
         y = self.relu(self.in5(self.deconv2(y)))
         y = self.tanh(self.deconv3(y))
-        y *= 150.0
+        y = y * 150.0
         return y
 
 

neuralstyle/utils.py
@@ -7,15 +7,27 @@ from torch.utils.serialization import load_lua
 from vgg16 import Vgg16
 
 import numpy as np
+from PIL import Image
 from scipy.misc import imread, imsave, imresize
 from scipy.ndimage.filters import median_filter
 
 
-def subtract_imagenet_mean(img):
-    """Subtract ImageNet mean pixel-wise from a BGR image."""
-    img[0, :, :] -= 103.939
-    img[1, :, :] -= 116.779
-    img[2, :, :] -= 123.68
+# result: RGB CxHxW [0,255] torch.FloatTensor
+def tensor_load_rgbimage(filename, size=None):
+    img = Image.open(filename)
+    if size is not None:
+        img = img.resize((size, size), Image.ANTIALIAS)
+    img = np.array(img).transpose(2, 0, 1)
+    img = torch.from_numpy(img).float()
+    return img
+
+
+def gram_matrix(y):
+    (b, ch, h, w) = y.size()
+    features = y.view(b, ch, w * h)
+    features_t = features.transpose(1, 2)
+    gram = features.bmm(features_t) / (ch * h * w)
+    return gram
 
 
 def add_imagenet_mean(img):
@@ -23,45 +35,6 @@ def add_imagenet_mean(img):
     img[0, :, :] += 103.939
     img[1, :, :] += 116.779
     img[2, :, :] += 123.68
-
-
-def load_and_preprocess_img(filename, size=None, center_crop=False):
-    """Load an image, and pre-process it as needed by models."""
-    try:
-        img = imread(filename, mode="RGB")
-    except OSError as e:
-        print(e)
-        sys.exit(1)
-
-    if center_crop:
-        # Extract a square crop from the center of the image.
-        cur_shape = img.shape[:2]
-        shorter_side = min(cur_shape)
-        longer_side_xs = max(cur_shape) - shorter_side
-        longer_side_start = int(longer_side_xs / 2.)
-        longer_side_slice = slice(longer_side_start, longer_side_start + shorter_side)
-        if shorter_side == cur_shape[0]:
-            img = img[:, longer_side_slice, :]
-        else:
-            img = img[longer_side_slice, :, :]
-
-    if size is not None:
-        # Resize the image.
-        cur_shape = img.shape[:2]
-        shorter_side = min(cur_shape)
-        aspect = max(cur_shape) / float(shorter_side)
-        new_shorter_side = int(size / aspect)
-        if shorter_side == cur_shape[0]:
-            new_shape = (new_shorter_side, size)
-        else:
-            new_shape = (size, new_shorter_side)
-        img = imresize(img, new_shape)
-
-    # Bring the color dimension to the front, convert to BGR.
-    img = img.transpose((2, 0, 1))[::-1].astype(float)
-
-    subtract_imagenet_mean(img)
-    return img[np.newaxis, :]
 
 
 def deprocess_img_and_save(img, filename):
@@ -76,6 +49,25 @@ def deprocess_img_and_save(img, filename):
     except OSError as e:
         print(e)
         sys.exit(1)
+
+
+def subtract_imagenet_mean_batch(batch):
+    """Subtract ImageNet mean pixel-wise from a BGR image."""
+    tensortype = type(batch)
+    mean = tensortype(batch.size())
+    mean[:, 0, :, :] = 103.939
+    mean[:, 1, :, :] = 116.779
+    mean[:, 2, :, :] = 123.680
+    batch -= mean
+
+
+def preprocess_batch(batch):
+    batch = batch.transpose(0, 1)
+    (r, g, b) = torch.chunk(batch, 3)
+    batch = torch.cat((b, g, r))
+    batch = batch.transpose(0, 1)
+    subtract_imagenet_mean_batch(batch)
+    return batch
 
 
 def init_vgg16(model_folder):