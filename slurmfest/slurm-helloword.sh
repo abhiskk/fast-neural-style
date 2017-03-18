1bb5ce4d3fabe86f02a6d0b0c6a9f0f5c7189c6e
darkstar112358 <abhishekkadiyan@gmail.com>
2017-03-18 19:53:16 -0400

    Add sample slurm file

---
+++ slurmfest/slurm-helloword.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+#SBATCH --job-name=ak_experiment
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=23:00:00
+#SBATCH --mem=200000
+#SBATCH --output=ak_experiment_%A.out
+#SBATCH --error=ak_experiment_%A.err
+#SBATCH --mail-user=ak6179@nyu.edu
+
+# Log what we're running and where.
+echo $SLURM_JOBID - `hostname` >> ~/slurm-jobs.txt
+
+module purge
+module load python3/intel/3.5.3
+
+python3 -m pip install -U pip setuptools --user
+python3 -m pip install cmake --user
+python3 -m pip install numpy --upgrade --user
+python3 -m pip install scipy --upgrade --user
+python3 -m pip install git+https://github.com/pytorch/pytorch.git@master  --user
+python3 -m pip install torchvision --user
+
+cd /scratch/ak6179/testing
+
+python3 -u helloworld.py > helloworld.log
