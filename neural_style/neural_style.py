dc75e152c21e5f17fd007e6f9565b35e792624ba
darkstar112358 <abhishekkadiyan@gmail.com>
2017-03-21 13:05:09 -0400

    Refactor

--- neural_style/neural_style.py
+++ neural_style/neural_style.py
@@ -39,7 +39,9 @@ def train(args):
     print("STYLE WEIGHT:", args.style_weight)
     print("DATASET:", args.dataset)
     print("CHECKPOINT DIR:", args.checkpoint_dir)
+    print("VALIDATION:", args.validation)
     print("VAL DIR:", args.val_dir)
+    print("STYLE SIZE:", args.style_size)
     print("=====================\n")
 
     transform = transforms.Compose([transforms.Scale(args.image_size),
@@ -83,11 +85,14 @@ def train(args):
             x = Variable(utils.preprocess_batch(x))
             if args.cuda:
                 x = x.cuda()
-            # pass images through the TransformerNet
+
             y = transformer(x)
-            features_y = vgg(y)
+
             xc = Variable(x.data.clone(), volatile=True)
+
+            features_y = vgg(y)
             features_xc = vgg(xc)
+
             f_xc_c = Variable(features_xc[1].data, requires_grad=False)
 
             content_loss = args.content_weight * mse_loss(features_y[1], f_xc_c)
@@ -114,26 +119,38 @@ def train(args):
                     agg_content_loss / (batch_id + 1),
                     agg_style_loss / (batch_id + 1)
                 )
-                if args.cuda:
-                    utils.deprocess_img_and_save(x.cpu().data.numpy(),
-                                                 args.val_dir + "/epoch_original_" + str(e + 1) + "_"
-                                                 + str(time.ctime()).replace(' ', '_') + ".jpg"
-                                                 )
-                    utils.deprocess_img_and_save(y.cpu().data.numpy(),
-                                                 args.val_dir + "/epoch_stylized_" + str(e + 1) + "_"
-                                                 + str(time.ctime()).replace(' ', '_') + ".jpg")
-                else:
-                    utils.deprocess_img_and_save(x.data.numpy(),
-                                                 args.val_dir + "/epoch_original_" + str(e + 1) + "_"
-                                                 + str(time.ctime()).replace(' ', '_') + ".jpg"
-                                                 )
-                    utils.deprocess_img_and_save(y.data.numpy(),
-                                                 args.val_dir + "/epoch_stylized_" + str(e + 1) + "_"
-                                                 + str(time.ctime()).replace(' ', '_') + ".jpg")
+                if args.validation:
+                    if args.cuda:
+                        utils.deprocess_img_and_save(x.cpu().data.numpy(),
+                                                     args.val_dir + "/epoch_original_" + str(e + 1) + "_"
+                                                     + str(time.ctime()).replace(' ', '_') + "_"
+                                                     + str(args.content_weight) + "_" + str(args.style_weight)
+                                                     + ".jpg"
+                                                     )
+                        utils.deprocess_img_and_save(y.cpu().data.numpy(),
+                                                     args.val_dir + "/epoch_stylized_" + str(e + 1) + "_"
+                                                     + str(time.ctime()).replace(' ', '_') + "_"
+                                                     + str(args.content_weight) + "_" + str(args.style_weight)
+                                                     + ".jpg")
+                    else:
+                        utils.deprocess_img_and_save(x.data.numpy(),
+                                                     args.val_dir + "/epoch_original_" + str(e + 1) + "_"
+                                                     + str(time.ctime()).replace(' ', '_') + "_"
+                                                     + str(args.content_weight) + "_" + str(args.style_weight)
+                                                     + ".jpg"
+                                                     )
+                        utils.deprocess_img_and_save(y.data.numpy(),
+                                                     args.val_dir + "/epoch_stylized_" + str(e + 1) + "_"
+                                                     + str(time.ctime()).replace(' ', '_') + "_"
+                                                     + str(args.content_weight) + "_" + str(args.style_weight)
+                                                     + ".jpg")
                 print(mesg)
 
 
-        torch.save(transformer, args.checkpoint_dir + "/epoch_" + str(e + 1) + ".model")
+        torch.save(transformer, args.checkpoint_dir + "/epoch_" + str(e + 1) + "_"
+                   + str(time.ctime()).replace(' ', '_') + "_"
+                   + str(args.content_weight) + "_" + str(args.style_weight)
+                   + ".model")
 
     print("\nDone :)")
 
@@ -171,21 +188,22 @@ def stylize(args):
 
 def main():
     parser = argparse.ArgumentParser(description="parser for fast-neural-style")
-    parser.add_argument("--val-dir", type=str, default='val-dir')
+    parser.add_argument("--validation", type=int, required=True)
+    parser.add_argument("--val-dir", type=str, default=None)
     parser.add_argument("--batch-size", "-b", type=int, default=4)
     parser.add_argument("--epochs", "-e", type=int, default=2)
-    parser.add_argument("--vgg-model", "-m", type=str, default="vgg-model")
+    parser.add_argument("--vgg-model", "-m", type=str, required=True)
     parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--cuda", type=int, default=0)
-    parser.add_argument("--dataset", type=str, default="MSCOCO")
+    parser.add_argument("--cuda", type=int, required=True)
+    parser.add_argument("--dataset", type=str, required=True)
     parser.add_argument("--image-size", type=int, default=256)
-    parser.add_argument("--style-size", default=None)
+    parser.add_argument("--style-size", type=int, default=None)
     parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--style-image", type=str, default="images/style-images/mosaic.jpg")
     parser.add_argument("--content-weight", type=float, default=1.)
-    parser.add_argument("--style-weight", type=float, default=5.)
+    parser.add_argument("--style-weight", type=float, default=100.)
     parser.add_argument("--log-interval", type=int, default=500)
-    parser.add_argument("--checkpoint-dir", type=str, default="checkpoints")
+    parser.add_argument("--checkpoint-dir", type=str, required=True)
     parser.add_argument("--train", type=int, default=1)
     parser.add_argument("--saved-model-path", type=str, default=None)
     parser.add_argument("--content-image", type=str, default=None)
