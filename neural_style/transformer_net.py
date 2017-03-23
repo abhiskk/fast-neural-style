Implement Instance Normalization, fixes #2

neural_style/transformer_net.py
@@ -9,11 +9,11 @@ class TransformerNet(torch.nn.Module):
 
         # Initial convolution layers
         self.conv1 = nn.Conv2d(3, 32, kernel_size=9, stride=1, padding=4)
-        self.bn1 = nn.BatchNorm2d(32)
+        self.in1 = InstanceNormalization(32)
         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1)
-        self.bn2 = nn.BatchNorm2d(64)
+        self.in2 = InstanceNormalization(64)
         self.conv3 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
-        self.bn3 = nn.BatchNorm2d(128)
+        self.in3 = InstanceNormalization(128)
 
         # Residual layers
         self.res1 = ResidualBlock(128)
@@ -24,27 +24,26 @@ class TransformerNet(torch.nn.Module):
 
         # Upsampling Layers
         self.deconv1 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
-        self.bn4 = nn.BatchNorm2d(64)
+        self.in4 = InstanceNormalization(64)
         self.deconv2 = nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1)
-        self.bn5 = nn.BatchNorm2d(32)
+        self.in5 = InstanceNormalization(32)
         self.deconv3 = nn.Conv2d(32, 3, kernel_size=9, stride=1, padding=4)
 
         # Non-linearities
         self.relu = nn.ReLU()
-        self.tanh = nn.Tanh()
 
     def forward(self, X):
         in_X = X
-        y = self.relu(self.bn1(self.conv1(in_X)))
-        y = self.relu(self.bn2(self.conv2(y)))
-        y = self.relu(self.bn3(self.conv3(y)))
+        y = self.relu(self.in1(self.conv1(in_X)))
+        y = self.relu(self.in2(self.conv2(y)))
+        y = self.relu(self.in3(self.conv3(y)))
         y = self.res1(y)
         y = self.res2(y)
         y = self.res3(y)
         y = self.res4(y)
         y = self.res5(y)
-        y = self.relu(self.bn4(self.deconv1(y)))
-        y = self.relu(self.bn5(self.deconv2(y)))
+        y = self.relu(self.in4(self.deconv1(y)))
+        y = self.relu(self.in5(self.deconv2(y)))
         y = self.deconv3(y)
         return y
 
@@ -58,14 +57,45 @@ class ResidualBlock(torch.nn.Module):
     def __init__(self, channels):
         super(ResidualBlock, self).__init__()
         self.conv1 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(channels)
+        self.in1 = InstanceNormalization(channels)
         self.conv2 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(channels)
+        self.in2 = InstanceNormalization(channels)
         self.relu = nn.ReLU()
 
     def forward(self, x):
         residual = x
-        out = self.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
+        out = self.relu(self.in1(self.conv1(x)))
+        out = self.in2(self.conv2(out))
         out = out + residual
+        return out
+
+
+class InstanceNormalization(torch.nn.Module):
+    """InstanceNormalization
+    Improves convergence of neural-style.
+    ref: https://arxiv.org/pdf/1607.08022.pdf
+    """
+
+    def __init__(self, dim):
+        super(InstanceNormalization, self).__init__()
+        self.scale = nn.Parameter(torch.FloatTensor(dim))
+        self.shift = nn.Parameter(torch.FloatTensor(dim))
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        self.scale.data.uniform_()
+        self.shift.data.zero_()
+
+    def forward(self, x):
+        n = x.size(2) * x.size(3)
+        t = x.view(x.size(0), x.size(1), n)
+        mean = torch.mean(t, 2).unsqueeze(2).expand_as(x)
+        # Calculate the biased var. torch.var returns unbiased var
+        var = torch.var(t, 2).unsqueeze(2).expand_as(x) * ((n - 1) / float(n))
+        scale_broadcast = self.scale.unsqueeze(1).unsqueeze(1).unsqueeze(0)
+        scale_broadcast = scale_broadcast.expand_as(x)
+        shift_broadcast = self.shift.unsqueeze(1).unsqueeze(1).unsqueeze(0)
+        shift_broadcast = shift_broadcast.expand_as(x)
+        out = (x - mean) / torch.sqrt(var + 1e-9)
+        out = (out * scale_broadcast) + shift_broadcast
         return out