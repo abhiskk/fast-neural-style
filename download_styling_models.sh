df085308a7af0fdde7621ce83b2911f58c8d1c55
Thibault Ayanides <thibault.serti@gmail.com>
2021-02-09 19:14:47 +0100

    Add dockerfile, update readme (#31)

--- download_styling_models.sh
+++ download_styling_models.sh
@@ -1,2 +1,3 @@
-wget https://www.dropbox.com/s/gtwnyp9n49lqs7t/saved-models.zip?dl=1
-unzip saved-models.zip?dl=1
+#!/bin/bash
+wget https://www.dropbox.com/s/gtwnyp9n49lqs7t/saved-models.zip?dl=1 -O saved-models.zip
+unzip saved-models.zip
